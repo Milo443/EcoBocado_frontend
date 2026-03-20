8863907ea97d8c15b67553dcace652ef7506e289
Camilo calderon <cacalderon@gane.com.co>
2026-03-20 11:16:48 -0500

    Fix font and icon asset processing for deployment

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import 'primereact/resources/themes/lara-light-green/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
 import 'chart.js/auto';
 import { PrimeReactProvider } from 'primereact/api';
 import { primeReactConfig } from './config/primereact';
